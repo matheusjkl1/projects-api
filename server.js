80b0f20c9b0afa1422822d837c135d17d5be97f7
matheusjkl1 <matheus.unilog@gmail.com>
2021-08-23 18:55:02 -0300

    add api

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(`${__dirname}/public`));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (_, res) => {
+  res.render('index');
+});
+
+http.listen(PORT, () => (`Servidor Ligado na porta ${PORT}`));
