4e64e702ddd1bde2c6c00a36d7fa315cf12ab2ee
matheusjkl1 <matheus.unilog@gmail.com>
2021-08-23 19:02:35 -0300

    fix styles

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const http = require('http').createServer(app);
 
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(`${__dirname}/public`));
 
