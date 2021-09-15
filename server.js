0a5a8d1b46cbf75714fed4c4cc4563f0743a91be
matheusjkl1 <matheus.unilog@gmail.com>
2021-09-14 21:04:31 -0300

    delete images

--- server.js
+++ server.js
@@ -24,16 +24,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.get('/banner', async (_, res) => {
-  const randomNumber = Math.floor(Math.random() * 10 + 1);
-
-  return res.status(200).json({ img: `uploads/${randomNumber}.png` });
-});
-
 app.get('/projects', async (_, res) => {
   const response = await modelProject.getAll();
 
-  if (!response) return res.status(400).json({ message: 'Nao a projetos' });
+  if (!response) return res.status(400).json({ message: 'Sem projetos' });
 
   return res.status(200).json(response);
 });
